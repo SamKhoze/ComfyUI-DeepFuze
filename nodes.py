20ab8904cc70fb3bc218ca581def11d53bf4b5f5
Sam Khoze <68170403+SamKhoze@users.noreply.github.com>
2024-07-10 23:13:47 +0530

    Deepfuze updated

--- nodes.py
+++ nodes.py
@@ -515,7 +515,6 @@ class DeepFuzeFaceSwap:
             "required": {
                 "source_images": ("IMAGE",),
                 "target_images": ("IMAGE",),
-				"audio": ("AUDIO",),
                 "enhancer": ("None,codeformer,gfpgan_1.2,gfpgan_1.3,gfpgan_1.4,gpen_bfr_256,gpen_bfr_512,gpen_bfr_1024,gpen_bfr_2048,restoreformer_plus_plus".split(","),{"default":'None'}),
                 "faceswap_model":("blendswap_256,inswapper_128,inswapper_128_fp16,simswap_256,simswap_512_unofficial,uniface_256".split(","),{"default":"blendswap_256"}),
                 "frame_enhancer": ("None,clear_reality_x4,lsdir_x4,nomos8k_sc_x4,real_esrgan_x2,real_esrgan_x2_fp16,real_esrgan_x4,real_esrgan_x4_fp16,real_hatgan_x4,span_kendata_x4,ultra_sharp_x4".split(","),{"default":'None'}),
@@ -533,6 +532,7 @@ class DeepFuzeFaceSwap:
 
             },
             "optional": {
+                "audio": ("AUDIO",),
                 "meta_batch": ("VHS_BatchManager",),
                 "loop_count": ("INT", {"default": 0, "min": 0, "max": 100, "step": 1}),
                 "filename_prefix": ("STRING", {"default": "deepfuze"}),
@@ -561,7 +561,6 @@ class DeepFuzeFaceSwap:
         self,
         source_images,
         target_images,
-		audio,
         enhancer,
         faceswap_model,
         frame_enhancer,
@@ -573,7 +572,8 @@ class DeepFuzeFaceSwap:
         face_mask_padding_top,
         device,
         frame_rate: int,
-        loop_count: int,
+        audio="",
+        loop_count: int = 0,
         filename_prefix="deepfuze",
         format="video/h265-mp4",
         pingpong=False,
@@ -920,11 +920,12 @@ class DeepFuzeFaceSwap:
             # faceswap_filename = temp_file
 
         print(result.stderr)
-        audio_file = os.path.join(audio_dir,str(time.time()).replace(".","")+".wav")
-        torchaudio.save(audio_file,audio["waveform"][0],audio["sample_rate"])
-        subprocess.run(f"ffmpeg -i {faceswap_filename} -i {audio_file} -c copy {faceswap_filename.replace('.mp4','_.mp4')} -y".split())
-
-        return load_video_cv(faceswap_filename.replace('.mp4','_.mp4'),0,'Disabled',512,512,0,0,1)
+        if audio:
+            audio_file = os.path.join(audio_dir,str(time.time()).replace(".","")+".wav")
+            torchaudio.save(audio_file,audio["waveform"][0],audio["sample_rate"])
+            subprocess.run(f"ffmpeg -i {faceswap_filename} -i {audio_file} -c copy {faceswap_filename.replace('.mp4','_.mp4')} -y".split())
+            return load_video_cv(faceswap_filename.replace('.mp4','_.mp4'),0,'Disabled',512,512,0,0,1)
+        return load_video_cv(faceswap_filename,0,'Disabled',512,512,0,0,1)
 
 
 
